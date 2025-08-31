67035ca4b00cf3de19ee83023fb154a6df97af81
user <albertik_1098@mail.ru>
2025-08-31 20:48:50 +0500

    add db_helper dispose to app

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -1,11 +1,22 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from fastapi import FastAPI
 
 from core.config import settings
 
 from api import router as api_router
+from models.db_helper import db_helper
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    #startup
+    yield
+    # shutdown
+    await db_helper.engine()
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(api_router, prefix='/questions', tags='Questions')
 
 if __name__ == '__main__':
