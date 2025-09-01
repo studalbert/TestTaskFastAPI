2e1ec03b050591e1b2a6bb167af66f22385e0a7a
user <albertik_1098@mail.ru>
2025-09-01 15:22:34 +0500

    include answers router to main

--- fastapi-app/api/__init__.py
+++ fastapi-app/api/__init__.py
@@ -1,3 +1,6 @@
 from fastapi import APIRouter
-
+from .views_questions import router as router_1
+from .views_answers import router as router_2
 router = APIRouter()
+router.include_router(router_1, prefix='/questions', tags=['Questions'])
+router.include_router(router_2, tags=['Answers'])
